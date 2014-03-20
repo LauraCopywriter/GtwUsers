f3f0ca589e1fbc90a09ff0d00dc1a3526e90e594
Sushil <jadavsushil@gmail.com>
2014-03-20 10:06:22 +0530

    - Added Code for Email validation - Added Client Side code for all field validation - Added Code for Unique email address and display message to user

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -16,13 +16,18 @@ class UsersController extends AppController {
     
     public function beforeFilter() {
         parent::beforeFilter();
-        $this->Auth->allow('signup', 'signin', 'signout', 'confirmation');
+        
+        $this->Auth->allow('signup', 'signin', 'signout', 'confirmation','forgot_password','reset_password');
+        
+        if( $this->Session->read('Auth.User') ){
+            $this->Auth->allow('edit');
+        }
         
         if( is_null( Configure::read('Gtw.admin_mail') ) ){
             echo 'Users plugin configuration error'; exit;
         }
     }
-
+    
     public function index() {
         $this->User->recursive = 0;
         $this->set('users', $this->paginate());
@@ -44,8 +49,11 @@ class UsersController extends AppController {
         return $this->redirect(array('action' => 'index'));
     }
     
-    public function edit($id = null) {
-        $this->User->id = $id;
+    public function edit() {
+    
+        $user = $this->Session->read('Auth.User');
+        $this->User->id = $user['id'];
+        
         if (!$this->User->exists()) {
             throw new NotFoundException(__('Invalid user'));
         }
@@ -56,7 +64,7 @@ class UsersController extends AppController {
             }
             $this->Session->setFlash(__('The user could not be saved. Please, try again.'));
         } else {
-            $this->request->data = $this->User->safeRead(null, $id);
+            $this->request->data = $this->User->safeRead(null, $this->User->id);
         }
         if (CakePlugin::loaded('GtwFiles')){
             $this->render('/Users/edit_avatar');
@@ -108,14 +116,13 @@ class UsersController extends AppController {
                     'plugin' => 'BoostCake',
                     'class' => 'alert-success'
                 ));
+                return $this->redirect($this->Auth->redirectUrl());
             }else{
                 $this->Session->setFlash('Error creating your account, please contact an administrator', 'alert', array(
                     'plugin' => 'BoostCake',
                     'class' => 'alert-danger'
                 ));
-            }
-            
-            return $this->redirect($this->Auth->redirectUrl());
+            }                        
         }
     }
     
@@ -124,7 +131,13 @@ class UsersController extends AppController {
         
         if($userId || $token){
             $user = $this->User->confirmation($userId, $token);
-            if (isset($user) && $this->Auth->login($user['User'])) {
+            if(!empty($user['User']['validated'])){
+                   $this->Session->setFlash('Your email address is already validated, please use email and password to login', 'alert', array(
+                    'plugin' => 'BoostCake',
+                    'class' => 'alert-danger'
+                ));
+                return $this->redirect($this->Auth->redirectUrl());
+            }elseif (isset($user) && $this->Auth->login($user['User'])) {
                 $this->Session->setFlash('Email address successfuly validated', 'alert', array(
                     'plugin' => 'BoostCake',
                     'class' => 'alert-success'
@@ -166,5 +179,69 @@ class UsersController extends AppController {
             $this->User->File->delete($oldFile);
         }
     }
+    public function forgot_password(){
+        //Check For Already Logged In
+        if ($this->Auth->login()){
+             return $this->redirect($this->Auth->redirectUrl());           
+        }    
+        $this->layout = false;
+        if ($this->request->is('post')){
+            $arrResponse = $this->User->ForgotPasswordEmail($this->request->data['User']['email']);
+            if(!empty($arrResponse)){
+                   if($arrResponse['status']=='fail'){
+                     $this->Session->setFlash($arrResponse['message'], 'alert', array(
+                            'plugin' => 'BoostCake',
+                            'class' => 'alert-danger'
+                       ));        
+                   }else{
+                       $this->Session->setFlash($arrResponse['message'], 'alert', array(
+                            'plugin' => 'BoostCake',
+                            'class' => 'alert-success'
+                       ));
+                       return $this->redirect($this->Auth->redirectUrl());
+                   }
+            }
+        }
+    }
     
+    public function reset_password($userId = null, $token = null){
+        $this->layout = false;        
+        if($userId && $token){
+            $arrResponse = $this->User->checkForgotPassword($userId,$token);
+            if($arrResponse['status']=='fail'){
+                $this->Session->setFlash($arrResponse['message'], 'alert', array(
+                    'plugin' => 'BoostCake',
+                    'class' => 'alert-danger'
+                ));
+                return $this->redirect($this->Auth->loginAction);
+            }                        
+        }else{
+            $this->Session->setFlash('Invalid Token', 'alert', array(
+                    'plugin' => 'BoostCake',
+                    'class' => 'alert-danger'
+                ));
+            return $this->redirect($this->Auth->loginAction);
+        }
+        $this->set(compact('userId','token'));
+        if ($this->request->is('post')) {
+            if($this->request->data['User']['new_password'] != $this->request->data['User']['new_password']){
+                $this->Session->setFlash('New Password and Confirm Password must be same', 'alert', array(
+                    'plugin' => 'BoostCake',
+                    'class' => 'alert-danger'
+                ));
+            }else{
+                $this->request->data['User']['id'] = $userId; 
+                $this->request->data['User']['password'] = $this->request->data['User']['new_password'];
+                $this->request->data['User']['token'] = md5(uniqid(rand(),true));
+                $this->request->data['User']['token_creation'] = date("Y-m-d H:i:s");
+                $this->User->save($this->request->data);
+                
+                $this->Session->setFlash('Your password has been updated successfully', 'alert', array(
+                    'plugin' => 'BoostCake',
+                    'class' => 'alert-success'
+                ));
+                return $this->redirect($this->Auth->loginAction);
+            }
+        }
+    }
 }
