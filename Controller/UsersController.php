316e3fe175a964afdb2410374af117b5e79f384d
jayapaliwal <jaya.paliwal28@gmail.com>
2015-02-11 02:37:05 +0530

    send email directly for logged in user

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -396,8 +396,9 @@ class UsersController extends AppController {
      * @return void
      */
     public function resend_verification() {
-        if ($this->request->is('post')) {
-            $arrResponse = $this->User->ResendVerification($this->request->data['User']['email']);
+        if ($this->request->is('post') || $this->Session->read('Auth.User.email')) {
+        	$email = ($this->Session->read('Auth.User.email')) ? $this->Session->read('Auth.User.email') : $this->request->data['User']['email'];
+        	$arrResponse = $this->User->ResendVerification($email);
             if (!empty($arrResponse)) {
                 if ($arrResponse['status'] == 'fail') {
                     $this->Session->setFlash($arrResponse['message'], 'alert', array(
